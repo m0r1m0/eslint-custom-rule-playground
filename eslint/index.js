719d3a2df63aaa264564b7cda92abf47da013c4a
pywd <yuki.wd7@gmail.com>
2021-02-20 18:47:50 +0900

    fix: ルールexport忘れ

--- eslint/index.js
+++ eslint/index.js
@@ -1,7 +1,9 @@
 const requireTranslate = require("./rules/require-translate");
+const useUtilFetch = require("./rules/use-util-fetch");
 
 module.exports = {
   rules: {
-    "require-translate": requireTranslate
+    "require-translate": requireTranslate,
+    "use-util-fetch": useUtilFetch,
   },
 };
